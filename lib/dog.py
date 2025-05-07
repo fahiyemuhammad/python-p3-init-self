1cacc52efb8ea31f270177dbf5ecbfd1547955a4
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-07 14:57:22 +0300

    writing the init dunder method

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+        print("A dog is born!")
+
+    def bark(self):
+        print("Woof!") 
+
+    def showing_self(self):
+        return self       
+
+fido = Dog("Fido")
+
+fido.showing_self()
+       
